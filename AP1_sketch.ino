036d2364b91ffc11a2bfd7f2842ce2e8df577ac9
Marissa Kwon <kwonma@oregonstate.edu>
2018-10-14 14:05:33 -0700

    Add files via upload

--- AP1_sketch.ino
+++ AP1_sketch.ino
@@ -1,15 +1,15 @@
 // VARIABLE
 // put period variable here
-int period = 1000;  // milliseconds
+int period = 10000;  // milliseconds
 // put static 2D array of
-int arr[7][7] = { {0,62,73,73,46,0}, //G
-                  {0,0,65,127,75,0,0}, //I
-                  {0,127,32,16,32,127,0}, //M
-                  {0,127,32,16,32,127,0}, //M
-                  {0,0,65,127,75,0,0}, //I
-                  {0,62,65,65,65,0,0}, //C
-                  {0,127,8,20,34,65,0} //K
-                };  
+int arr[7][7] = { {0, 62, 73, 73, 46, 0}, //G
+  {0, 0, 65, 127, 75, 0, 0}, //I
+  {0, 127, 32, 16, 32, 127, 0}, //M
+  {0, 127, 32, 16, 32, 127, 0}, //M
+  {0, 0, 65, 127, 75, 0, 0}, //I
+  {0, 62, 65, 65, 65, 0, 0}, //C
+  {0, 127, 8, 20, 34, 65, 0} //K
+};
 
 
 // assign digital pins for LEDs;
@@ -21,6 +21,9 @@ int row4 = 8;
 int row5 = 9;
 int row6 = 10;
 
+unsigned long increment =  (period / (49.0 * 2.0)); // delay increment
+
+
 void setup() {
   // sets the baud rate for serial input and output
   // we need this in case we want to debug on the serial monitor
@@ -28,7 +31,6 @@ void setup() {
 
   // take period and divide it by 7 letters * 7 lights per letter
   // divide it by 2 because message should be displayed fully in 1/2 period
-  unsigned long increment =  period / (49.0 * 2.0); // delay increment
 
   Serial.print("This is our time between pixels: ");
   Serial.println(increment);  // debugging purposes
@@ -53,13 +55,41 @@ void loop() {
     // one value in the array represents the whole jth row
     for (int j = 0; j < 7; j++) {
       int val = arr[i][j];
-      digitalWrite(row0, HIGH && (val & B0000001));
-      digitalWrite(row1, HIGH && (val & B0000010));
-      digitalWrite(row2, HIGH && (val & B0000100));
-      digitalWrite(row3, HIGH && (val & B0001000));
-      digitalWrite(row4, HIGH && (val & B0010000));
-      digitalWrite(row5, HIGH && (val & B0100000));
-      digitalWrite(row6, HIGH && (val & B1000000));
+      if (val & B0000001)
+        digitalWrite(row0, HIGH);
+      else
+        digitalWrite(row0, LOW);
+      if (val & B0000010)
+        digitalWrite(row1, HIGH);
+      else
+        digitalWrite(row1, LOW);
+      if (val & B0000100)
+        digitalWrite(row2, HIGH);
+      else
+        digitalWrite(row2, LOW);
+      if (val & B0001000)
+        digitalWrite(row3, HIGH);
+      else
+        digitalWrite(row3, LOW);
+      if (val & B0010000)
+        digitalWrite(row4, HIGH);
+      else
+        digitalWrite(row4, LOW);
+      if (val & B0100000)
+        digitalWrite(row5, HIGH);
+      else
+        digitalWrite(row5, LOW);
+      if (val & B1000000)
+        digitalWrite(row6, HIGH);
+      else
+        digitalWrite(row6, LOW);
+      Serial.print(HIGH && (val & B0000001));
+      Serial.print(HIGH && (val & B0000010));
+      Serial.print(HIGH && (val & B0000100));
+      Serial.print(HIGH && (val & B0001000));
+      Serial.print(HIGH && (val & B0010000));
+      Serial.print(HIGH && (val & B0100000));
+      Serial.println(HIGH && (val & B1000000));
     }
     delay(increment);
   }
@@ -69,16 +99,45 @@ void loop() {
   for (int i = 6; i >= 0; i--) {
     // go through the 2d array to get each row (7) of pixels
     // one value in the array represents the whole jth row
-    for (int j = 0; j < 7; j++) {
+    for (int j = 6; j >= 0; j--) {
       int val = arr[i][j];
-      digitalWrite(row0, HIGH && (val & B0000001));
-      digitalWrite(row1, HIGH && (val & B0000010));
-      digitalWrite(row2, HIGH && (val & B0000100));
-      digitalWrite(row3, HIGH && (val & B0001000));
-      digitalWrite(row4, HIGH && (val & B0010000));
-      digitalWrite(row5, HIGH && (val & B0100000));
-      digitalWrite(row6, HIGH && (val & B1000000));
+      if (val & B0000001)
+        digitalWrite(row0, HIGH);
+      else
+        digitalWrite(row0, LOW);
+      if (val & B0000010)
+        digitalWrite(row1, HIGH);
+      else
+        digitalWrite(row1, LOW);
+      if (val & B0000100)
+        digitalWrite(row2, HIGH);
+      else
+        digitalWrite(row2, LOW);
+      if (val & B0001000)
+        digitalWrite(row3, HIGH);
+      else
+        digitalWrite(row3, LOW);
+      if (val & B0010000)
+        digitalWrite(row4, HIGH);
+      else
+        digitalWrite(row4, LOW);
+      if (val & B0100000)
+        digitalWrite(row5, HIGH);
+      else
+        digitalWrite(row5, LOW);
+      if (val & B1000000)
+        digitalWrite(row6, HIGH);
+      else
+        digitalWrite(row6, LOW);
+      //digitalWrite(row0, (val & B0000001));
+      //digitalWrite(row1, (val & B0000010));
+      //digitalWrite(row2, (val & B0000100));
+      //digitalWrite(row3, (val & B0001000));
+      //digitalWrite(row4, (val & B0010000));
+      //digitalWrite(row5, (val & B0100000));
+      //digitalWrite(row6, (val & B1000000));
     }
     delay(increment);
   }
 }
+
